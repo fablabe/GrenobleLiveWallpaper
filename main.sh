989fc0e1efcf681b1999bb2ae1b23348615a45fe
leo <leomarty@protonmail.com>
2021-05-01 23:17:05 +0200

    etend l'image aux deux écrans

--- main.sh
+++ main.sh
@@ -2,8 +2,8 @@
 
 while [ true ]
 do  
-    wget -q -O bg.jpg $(python scraper.py) 
-    feh --bg-fill bg.jpg
+    wget -q -O bg.jpg $(python ./scraper.py) 
+    feh --bg-fill --no-xinerama bg.jpg
     rm bg.jpg
     sleep 600
 done
