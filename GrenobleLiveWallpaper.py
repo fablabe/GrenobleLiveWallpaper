d6362ec43e10e2123311aacbc9fd26499af4ae1b
Yann Bourdin <yann.bourdin@gmail.com>
2021-05-21 19:00:20 +0200

    ok la c pa mal

--- GrenobleLiveWallpaper.py
+++ GrenobleLiveWallpaper.py
@@ -4,23 +4,30 @@ import platform
 _platform = platform.system()
 import time, uptime
 
-if os.path.exists('.pid'):
-	if time.time() - uptime.uptime() < os.path.getmtime('.pid') : # si le fichier a été modifié après le démarrage de l'OS
-		c = input("Grenoble Live Wallpaper is already running. Do you wish to kill it? ([y]/n) ")
-		if c in ['', 'y', 'Y']:
-			with open('.pid', 'r') as pidfile:
-				pid = int(pidfile.readline())
-				print(pid)
-				if _platform == 'Windows':
-					subprocess.call(['taskkill', '/F', '/T', '/PID',  str(pid)])
-				else:
-					os.kill(pid, signal.SIGTERM)
-			os.remove('.pid')
-	else :
-		os.remove('.pid')
-
-else :
+def start():
 	p = subprocess.Popen(["python", "main.pyw"])
 	print("Grenoble Live Wallpaper started.")
 	with open('.pid', 'w') as pidfile:
 		pidfile.write(str(p.pid))
+
+if __name__ == '__main__':
+
+	if os.path.exists('.pid'):
+		if time.time() - uptime.uptime() < os.path.getmtime('.pid') : # si le fichier a été modifié après le démarrage de l'OS
+			c = input("Grenoble Live Wallpaper is already running. Do you wish to kill it? ([y]/n) ")
+			if c in ['', 'y', 'Y']:
+				with open('.pid', 'r') as pidfile:
+					pid = int(pidfile.readline())
+					print(pid)
+					if _platform == 'Windows':
+						subprocess.call(['taskkill', '/F', '/T', '/PID',  str(pid)])
+					else:
+						os.kill(pid, signal.SIGTERM)
+				os.remove('.pid')
+		else :
+			os.remove('.pid')
+			start()
+	else :
+		start()
+
+
